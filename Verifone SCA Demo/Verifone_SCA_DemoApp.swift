a184d3a19b089de10f011cd63b90b70146a95287
Shawn Roller <sroller@techstyle.com>
2021-08-29 17:36:57 -0400

    Initial Commit

---
+++ Verifone SCA Demo/Verifone_SCA_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Verifone_SCA_DemoApp.swift
+//  Verifone SCA Demo
+//
+//  Created by Shawn Roller on 8/29/21.
+//
+
+import SwiftUI
+
+@main
+struct Verifone_SCA_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
